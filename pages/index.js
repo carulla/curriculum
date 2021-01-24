89db267ad57f970ceefe8e3b93f9914e0b53ba46
carulla <77812094+carulla@users.noreply.github.com>
2021-01-24 16:31:54 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+function Home(){
+	return <html>	
+				<title>Curriculum Vitae</title>
+				<link rel="stylesheet" type="text/css" href="css.css"/>
+				<style type="text/css">git 
+						
+					</style>
+					<body>		
+						<header id="titulo" align="center">Gabriel Carulla Abucater de Santana</header>
+								<h2 id="subtitulo" align="center"><b>Professor de Inglês, Jornalista, Desenvolvedor Web, Programador</b></h2>
+					<div id="split1">
+						<div id="left1">
+							<aside id="r">IMG</aside>
+						</div>
+						<div id="right1">
+							<article id="resumo">Graduando em Jornalismo pela Universidade Federal da Paraíba e graduando em Letras Inglês pela Faculdade Estácio, atua como redator colaborador na coluna cultural do Jornal Brasil de Fato. Tem experiência na área de Comunicação, com ênfase em Teoria e Ética do Jornalismo e Marketing Digital. Certificado em inglês avançado pela Universidade de Cambridge (CAE), atuou como monitor de inglês na Cultura Inglesa Tambauzinho entre 2019-2020 e como monitor acadêmico na disciplina de Relações Étnico Raciais do Departamento de Jornalismo da UFPB em 2020. É vinculado ao Setor de Estudos em Cidadania e Crítica Social (SECTS/DGP/CNPq) da UFPB, assim como ao Grupo de Estudos em Filosofia e Crítica Social (GEFICS/UFPB/DGP/CNPq). Interessado por ciência de dados e desenvolvimento Web, estuda linguagens e lógica de programação com ênfase em JavaScript e Python.</article>
+						</div>
+					</div>
+								<h3 id="exp"><b>EXPERIÊNCIA:</b></h3>
+								<h4 class="empresa" id="cultura"><b>Cultura Inglesa Tambauzinho, João Pessoa-PB — <i>Monitor</i></b></h4>
+						<p class="data">MARÇO DE 2019 - MARÇO DE 2020</p>
+						<i class="descricao">
+							Como professor do programa de tutoria para alunos com dificuldades de aprendizagem ou que necessitavam de reposição de conteúdos, teve a oportunidade de lidar
+							com alunos de diversas faixas etárias e níveis de inglês, desde crianças até idosos, do inglês básico ao intermediário.
+						</i>
+							<h4 class="empresa" id="bdf"><b>Jornal Brasil de Fato PB, João Pessoa-PB — <i>Redator Colaborador</i></b>
+						</h4>
+							<p class="data">DESDE JUNHO DE 2019</p>
+							<i class="descricao">
+							Como jornalista cultural, colabora com a coluna de cultura do Jornal Brasil de Fato PB, trazendo pautas do cenário artístico da capital paraibana e região metropolitana.</i>
+						<h4 class="empresa" id="barco"><b>
+							Embarcação Extremo Oriente, João Pessoa-PB  — <i>Social Media</i></b></h4>
+							<p class="data">FREELANCE</p>
+							<i class="descricao">
+							Como Social Media freelancer, produziu vídeos, artes personalizadas e textos como conteúdo de Marketing para a  empresa do ramo turístico.</i>
+							<h3 id="exp"><b>FORMAÇÃO:</b></h3>
+							<h4 class="empresa" id="ufpb"><b>
+							Universidade Federal da Paraíba, João Pessoa-PB — <i>Graduando</i></b></h4>
+							<p class="data">DESDE JULHO DE 2017</p>
+							<i class="descricao">
+							Estudante do 6º período do curso de Jornalismo da UFPB.</i>
+							<h4 class="empresa" id="estacio"><b>
+							Faculdade Estácio, João Pessoa - PB — <i>Graduando</i></b></h4>
+							<p class="data">DESDE DE MARÇO DE 2020</p>
+							<i class="descricao">
+							Estudante do 3º período do curso à distância de Letras Inglês da Faculdade Estácio.</i>
+					<div id="split">
+							<div id="left">
+								<p class="idiomas"><b>IDIOMAS:</b></p>
+										<div class="ingles">Inglês Avançado(C1)</div>
+										<div class ="ingles">Espanhol Intermediário(B2)</div>
+										<div class ="ingles">Francês Básico (A2)</div>
+									</div>
+							<div id="right">
+								<p class="idiomas1"><b>TÍTULOS:</b></p>
+										<i class="cert">🏆 Certificate in Advanced English(CAE) - Cambridge University of London ✔</i>
+										<div id="certificate">img</div>
+						</div>
+						</div>
+				  </body>
+   		 </html>
+	
+							}
+							export default Home
+							
+
